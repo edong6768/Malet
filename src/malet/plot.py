976cd196ebcb375edb0f80f9cf7ea067bc70997b
edong6768 <edong6768@gmail.com>
2024-07-10 21:24:28 +0900

    FIX avg_field when no seed in df in malet-plot

--- src/malet/plot.py
+++ src/malet/plot.py
@@ -168,7 +168,7 @@ def draw_metric(tsv_file, plot_config, save_name='', preprcs_df=lambda *x: x):
             avg_field = optimized_field = set()
         else:
             key_field       = {*x_fields, *pmlf, *pcrf, 'metric'}
-            avg_field       = {'seed'} if 'seed' not in key_field else set()
+            avg_field       = {'seed'} if 'seed' not in key_field and 'seed' in df.index.names else set()
             specified_field = {k for k in {*df.index.names} if len({*df.index.get_level_values(k)})==1}
             optimized_field = {*df.index.names} - key_field - avg_field - specified_field
             
